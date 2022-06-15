694bb444ea98d06c401ecb282065866a7950b687
Michael Alvarez <michael.alvarez687@myhunter.cuny.edu>
2022-06-15 14:15:40 -0400

    html vue, app header modified

---
+++ main-client/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <header class="header">
+        <h1 class="title">Hello?</h1>
+        <ul>
+            <li>
+                <nuxt-link to="/">Home</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to='/george'> George</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to='/about'> About</nuxt-link>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+};
+
+</script>
+
+<style>
+
+</style>
+
--- main-client/pages/index.vue
+++ main-client/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1> testing </h1>
+  </div>
 </template>
 
 <script lang="ts">
