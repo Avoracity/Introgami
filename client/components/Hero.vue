212a7ce4db561655f4ea38d8269ec1724c44b80b
Avoracity <avoracity7@gmail.com>
2022-07-26 22:53:40 -0400

    npm i -d concurrently | removes tailwind warnings

--- client/components/Hero.vue
+++ client/components/Hero.vue
@@ -1,19 +1,10 @@
 <template>
     <div class="Hero Container">
-        <h1 class="header">Hello?</h1>
-        <button class="button btn"> First button </button>
         <div class = "chat-notification"> miao </div>
-        <ul>
-            <li>
-                <nuxt-link to="/">Home</nuxt-link>
-            </li>
-            <li>
-                <nuxt-link to='/george'> George</nuxt-link>
-            </li>
-            <li>
-                <nuxt-link to='/about'> About</nuxt-link>
-            </li>
-        </ul>
+                <div class = "chat-notification"> miao </div>
+        <div class = "chat-notification"> miao </div>
+        <div class = "chat-notification"> miao </div>
+
     </div>
 </template>
 
