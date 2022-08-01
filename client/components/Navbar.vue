2f3c48304bede789b56351a88aa740cd13209322
Avoracity <avoracity7@gmail.com>
2022-08-01 12:22:10 -0400

    Navbar added

---
+++ client/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="Navbar Container">
+        <div class = "navbar"></div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+};
+
+</script>
+
+<style scoped src="@/assets/styles/navbar.css"> </style>
+
