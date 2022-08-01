d37ec3829ed88285394e43b97a64656636d615fb
Avoracity <avoracity7@gmail.com>
2022-08-01 12:15:37 -0400

    fresh folder restart

---
+++ client/components/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="Hero Container">
+        <div class = "hero"></div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+};
+
+</script>
+
+<style scoped src="@/assets/styles/hero.css"> </style>
+
---
+++ client/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+
+  <Hero/>
+
+  </div>
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
