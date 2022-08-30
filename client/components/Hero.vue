09fe7f635108008c8e374257beef0566d21b2f05
Avoracity <avoracity7@gmail.com>
2022-08-30 15:58:16 -0400

    text change + revert no-repeat and cover bg

--- client/components/Hero.vue
+++ client/components/Hero.vue
@@ -2,7 +2,7 @@
     <div class="hero-container">
         <div class = "hero hero-crane">
             <div class="hero-title"> introgami </div>
-            <div class="hero-description"> Origami for introverts </div>
+            <div class="hero-description"> Letters and Origamis for Introverts </div>
         </div>
     </div>
     
