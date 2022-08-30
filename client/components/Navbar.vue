b904eabb08c3b72ddaef675d1b348667c933e24c
Avoracity <avoracity7@gmail.com>
2022-08-30 16:34:25 -0400

    redirect button

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -2,7 +2,7 @@
     <div class="Navbar Container">
         <div class = "navbar">
             <div class = "btn_container">
-                <div class= "about_btn"> About</div>
+                <div class= "about_btn"  type="button" onclick=" window.location.href='index.vue' " > About</div>
                 <div class="explore_btn"> Explore </div>
                 <div class ="login_btn"> Login</div>
                 <div class="create_btn"> Create</div>
