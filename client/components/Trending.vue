a32f19fcae38ec630cd06d3455434aa6881b71d3
Avoracity <avoracity7@gmail.com>
2022-09-03 20:56:09 -0400

    trending title added

--- client/components/Trending.vue
+++ client/components/Trending.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="trending-container">
         <div class = "trending">
+
+            <div class="trending-title">
+                <div class="w-full min-h-24 rounded overflow-hidden shadow-lg">
+                    <div class="text-center font-bold text-xl mb-2">The Coldest Sunset</div>
+                </div>       
+            </div>
+
+     
+    
+
             <div class = "trending_wrapper">
         
                  <Card />
