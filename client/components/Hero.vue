aef8a94f5fc153c3278d04aa4a3ed04ec44ee313
Avoracity <avoracity7@gmail.com>
2022-12-15 20:54:31 -0500

    desktop container added

--- client/components/Hero.vue
+++ client/components/Hero.vue
@@ -2,13 +2,17 @@
     <div class="hero">
         <div class="hero-container">
             <img src="../assets/images/introgamibg.png" class="intro-bg" />
-            <div class="hero-title"> <h1>Introgami</h1> </div>
-            <div class="hero-description"> <h2>Letters and Origamis for Introverts </h2> </div>
+            <div class="hero-title">
+                <h1>Introgami</h1>
+            </div>
+            <div class="hero-description">
+                <h2>Letters and Origamis for Introverts </h2>
+            </div>
 
-        </div> 
+        </div>
 
     </div>
-    
+
 </template>
 
 <script>
@@ -18,9 +22,8 @@ export default {
 
 </script>
 
-<style lang="css" scoped> 
+<style lang="css" scoped>
 @import "../assets/styles/hero.css";
-@import "../assets/styles/global.css";
-
+ @import "../assets/styles/global.css";
 </style>
 
