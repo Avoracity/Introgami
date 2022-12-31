692216b71fa523c9530ca7e51c93b78a29a4b028
Avoracity <avoracity7@gmail.com>
2022-12-31 15:31:30 -0500

    basic footer and fontawesome installed

--- vite-project/components/Footer.tsx
+++ vite-project/components/Footer.tsx
@@ -1,20 +1,36 @@
-
 import React from "react";
 import "../assets/styles/footer.css";
 import "../assets/styles/global.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faYoutube, faTwitter, faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
 
-
-function Footer(){
-    return(
-        <div>
-            <div className="footer">
-                <div className = "footer-container"></div>
-            </div>
-            
-        </div>
-    )
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <ul className="flex-row footer-links">
+          <li><a href="#terms">Terms</a></li>
+          <li><a href="#privacy">Privacy</a></li>
+          <li><a href="#about-us">About Us</a></li>
+        </ul>
+        <div className="footer-spacer" />
+        <ul className="flex-row social-media-icons">
+          <li>
+          <FontAwesomeIcon icon={faYoutube} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faTwitter} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faInstagram} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faFacebook} />
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
-
-
