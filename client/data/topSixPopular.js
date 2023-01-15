2eb756f5f797c8eb5fd028303f6a73ffe08d16a9
Avoracity <avoracity7@gmail.com>
2023-01-15 17:47:42 -0500

    recent style and data added

---
+++ client/data/topSixPopular.js
@@ -0,0 +1,69 @@
+export default [
+    {
+        author:"--",
+        badges: [],
+        title:"--",
+        date_created:"--",
+        description:"--",
+        img_preview:"/client/assets/images/plaid-pattern.png",
+        likes:0,
+        link: "--"
+
+    },
+    {
+        author:"--",
+        badges: [],
+        title:"--",
+        date_created:"--",
+        description:"--",
+        img_preview:"/client/assets/images/plaid-pattern.png",
+        likes:0,
+        link: "--"
+
+    },
+    {
+        author:"--",
+        badges: [],
+        title:"--",
+        date_created:"--",
+        description:"--",
+        img_preview:"/client/assets/images/plaid-pattern.png",
+        likes:0,
+        link: "--"
+
+    },
+    {
+        author:"--",
+        badges: [],
+        title:"--",
+        date_created:"--",
+        description:"--",
+        img_preview:"/client/assets/images/plaid-pattern.png",
+        likes:0,
+        link: "--"
+
+    },
+    {
+        author:"--",
+        badges: [],
+        title:"--",
+        date_created:"--",
+        description:"--",
+        img_preview:"/client/assets/images/plaid-pattern.png",
+        likes:0,
+        link: "--"
+
+    },
+    {
+        author:"--",
+        badges: [],
+        title:"--",
+        date_created:"--",
+        description:"--",
+        img_preview:"/client/assets/images/plaid-pattern.png",
+        likes:0,
+        link: "--"
+
+    },
+  
+]
