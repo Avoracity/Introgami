a4e2a1f9289c911b81fb536c3f994ad0ff08a6cb
Avoracity <avoracity7@gmail.com>
2023-01-15 23:41:45 -0500

    accordion open/close feature fixed

--- client/components/QnA.vue
+++ client/components/QnA.vue
@@ -1,89 +1,51 @@
 <template>
-  <div class ="qna">
-    <div class="qna-container">
-
-<div class = "qna_bg">
-    <div class="cardboard_container">
-        
-        <div class="accordion">
-            <div class="accordion-item">
-                <div class="accordion-item-header">
-                    What is Introgami?
-                </div>
-                <div class="accordion-item-body">
-                    <div class="accordion-item-body-content">
-                        Introgami is an origami . feaieal
-                    </div>
-                </div>
-
+    <div>
+      <div class="accordion">
+        <div v-for="(faq, i) in faqs" :key="i">
+          <div class="accordion-item">
+            <div class="accordion-item-header" @click="toggleOpen(i)" :class="{ 'active': faq.open }">
+              {{ faq.question }}
             </div>
-        </div>
-
-            
-        <div class="accordion">
-            <div class="accordion-item">
-                <div class="accordion-item-header">
-                    What is xxx?
-                </div>
-                <div class="accordion-item-body">
-                    <div class="accordion-item-body-content">
-                        Introgami is an origami . feaieal
-                    </div>
-                </div>
-
+            <div class="accordion-item-body" v-if="faq.open">
+              <div class="accordion-item-body-content">
+                {{ faq.answer }}
+              </div>
             </div>
+          </div>
         </div>
-            
-        <div class="accordion">
-            <div class="accordion-item">
-                <div class="accordion-item-header">
-                    What is zzz?
-                </div>
-                <div class="accordion-item-body">
-                    <div class="accordion-item-body-content">
-                        Introgami is an origami . feaieal
-                    </div>
-                </div>
-
-            </div>
-        </div>
-            
-        <div class="accordion">
-            <div class="accordion-item">
-                <div class="accordion-item-header">
-                    What is yyy?
-                </div>
-                <div class="accordion-item-body">
-                    <div class="accordion-item-body-content">
-                        Introgami is an origami . feaieal
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
+      </div>
     </div>
-</div>
-</div>
-  </div>
-    
-</template>
-
-
-<script>
-
-export default {
-    
-    mounted() {
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        faqs: [
+          { id: 1, question: "What is Introgami?", answer: "Introgami is an origami platform.", open: false },
+          { id: 2, question: "How do I create an account?", answer: "You can create an account by clicking the 'Sign Up' button on the homepage.", open: false },
+          { id: 3, question: "Can I share my origami creations on Introgami?", answer: "Yes, you can share your origami creations on Introgami by creating a project and publishing it.", open: false }
+        ]
+      }
+    },
+    methods: {
+      toggleOpen(index) {
+        this.faqs = this.faqs.map((faq, i) => {
+          if (index === i) {
+            faq.open = !faq.open;
+          } else {
+            faq.open = false;
+          }
+          return faq;
+        });
+      }
     }
-}
-</script>
-
+  }
+  </script>
+   
 
 <style lang="css" scoped> 
 @import "@/assets/styles/qna.css";
 @import "@/assets/styles/global.css";
 
 </style>
-
-
--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -33,7 +33,7 @@ export default defineNuxtConfig({
       ],
       // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
       plugins: [
-        { src: '../client/assets/scripts/accordion.js', mode: 'client' }
+       
       ],
     
       // Auto import components: https://go.nuxtjs.dev/config-components
