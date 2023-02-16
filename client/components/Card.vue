2dd96d7fe141b270f7a5211a3ac651348a9a7e2d
Avoracity <malvarez@venturseed.com>
2023-02-16 17:35:58 -0500

    removed badge and mins float

--- client/components/Card.vue
+++ client/components/Card.vue
@@ -1,43 +1,34 @@
 
 <template>
-<div>
-    <div class="mt-8 grid lg:grid-cols-3 gap-4 pr-8 pb-8">
-          <!-- cards go here -->
-          <div class="card hover:shadow-lg"> 
-            <img src="@/assets/images/plaid-pattern-sundae.png" alt="stew" class="h-32 sm:h-48 w-full object-cover">
-            <div class="m-4">
-              <span class="font-bold">5 Bean Chili Stew</span>
-              <span class="block text-gray-500 text-sm">Recipe by Mario</span>
+  <div>
+      <div class="mt-8 grid lg:grid-cols-3 gap-4 pr-8 pb-8">
+            <!-- cards go here -->
+            <div class="card hover:shadow-lg"> 
+              <img src="@/assets/images/plaid-pattern-sundae.png" alt="stew" class="h-32 sm:h-48 w-full object-cover">
+              <div class="m-4">
+                <span class="font-bold">5 Bean Chili Stew</span>
+                <span class="block text-gray-500 text-sm">Recipe by Mario</span>
+              </div>
             </div>
-            <div class="badge">
-              <svg class="inline-block w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-              <span>25 mins</span>
+
+            <div class="card hover:shadow-lg"> 
+              <img src="@/assets/images/plaid-pattern-sundae.png" alt="noodles" class="h-32 sm:h-48 w-full object-cover">
+              <div class="m-4">
+                <span class="font-bold">Veg Noodles</span>
+                <span class="block text-gray-500 text-sm">Recipe by Mario</span>
+              </div>
             </div>
-          </div>
-          <div class="card hover:shadow-lg"> 
-            <img src="@/assets/images/plaid-pattern-sundae.png" alt="noodles" class="h-32 sm:h-48 w-full object-cover">
-            <div class="m-4">
-              <span class="font-bold">Veg Noodles</span>
-              <span class="block text-gray-500 text-sm">Recipe by Mario</span>
+
+            <div class="card hover:shadow-lg"> 
+              <img src="@/assets/images/plaid-pattern-sundae.png" alt="curry" class="h-32 sm:h-48 w-full object-cover">
+              <div class="m-4">
+                <span class="font-bold">Tofu Curry</span>
+                <span class="block text-gray-500 text-sm">Recipe by Mario</span>
+              </div>   
             </div>
-            <div class="badge">
-              <svg class="inline-block w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-              <span>25 mins</span>
-            </div>
-          </div>
-          <div class="card hover:shadow-lg"> 
-            <img src="@/assets/images/plaid-pattern-sundae.png" alt="curry" class="h-32 sm:h-48 w-full object-cover">
-            <div class="m-4">
-              <span class="font-bold">Tofu Curry</span>
-              <span class="block text-gray-500 text-sm">Recipe by Mario</span>
-            </div>
-            <div class="badge">
-              <svg class="inline-block w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-              <span>25 mins</span>
-            </div>
-          </div>
-        </div>
-</div>
+            
+      </div>
+  </div>
 
 </template>
 
